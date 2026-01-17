0ce1599ab6e232edc331220f4cf45f3613a6987b
vjbhaskar <vj.bhaskar012@gmail.com>
2026-01-17 16:45:29 +0100

    add types for api responses

---
+++ frontend/src/types/index.ts
@@ -0,0 +1,40 @@
+export interface ObjectType {
+  id: number;
+  labels: string[];
+  name: string;
+  properties: PropertiesType;
+}
+
+interface PropertiesType {
+  customer: string;
+  due_date: string;
+  item: string;
+  item_count: string;
+  order_id: string;
+  priority: string;
+  status: string;
+  zone: string;
+}
+
+export interface PoistionsType {
+  a: number;
+  altitude: number;
+  b: number;
+  battery: {
+    percentage: number;
+    percentage_last_update: string;
+  };
+  c: number;
+  flags: [];
+  is_valid: true;
+  latitude: number;
+  longitude: number;
+  object_id: number;
+  source_id: number;
+  tag_id: string;
+  tenant_id: 1;
+  timestamp: string;
+  x: number;
+  y: number;
+  z: number;
+}
